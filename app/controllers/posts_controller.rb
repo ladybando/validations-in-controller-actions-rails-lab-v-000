6d931b8dd07cf003190d5ea9e39e733f5d253afe
Ayana Bando <ayanaflax@yahoo.com>
2019-02-07 20:06:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,8 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    
+    redirect_to post_path(:edit)
   end
 
   private
